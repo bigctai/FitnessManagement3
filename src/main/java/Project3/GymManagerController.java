7cf4c014b046d40eb7d2dd4273a2ea9821a7d87a
Chris Tai <christai100154@gmail.com>
2022-10-31 17:02:38 -0400

    Finished draft of GUI for checkin/out

--- src/main/java/Project3/GymManagerController.java
+++ src/main/java/Project3/GymManagerController.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -12,15 +13,25 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
-public class GymManagerController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GymManagerController implements Initializable {
 
     ObservableList<String> locations = FXCollections.observableArrayList("Edison", "Bridgewater", "Franklin", "Piscataway", "Somerville");
+    String[] teachers = {"Davis", "Kim", "Denise", "Emma", "Jennifer"};
     @FXML
     ChoiceBox chooseLocation = new ChoiceBox();
-
     @FXML
-    public void initialize() {
-        chooseLocation.setItems(locations);
+    ChoiceBox chooseLocation1 = new ChoiceBox();
+    @FXML
+    ChoiceBox chooseTeacher = new ChoiceBox();
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){
+        chooseLocation.getItems().addAll(locations);
+        chooseLocation1.getItems().addAll(locations);
+        chooseTeacher.getItems().addAll(teachers);
     }
 
 }
--- src/main/java/Project3/GymManagerMain.java
+++ src/main/java/Project3/GymManagerMain.java
@@ -11,8 +11,9 @@ public class GymManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
+        fxmlLoader.setController(new GymManagerController());
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gym Manager");
         stage.setScene(scene);
         stage.show();
     }
