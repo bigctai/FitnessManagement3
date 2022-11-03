7d53f4493b4fd27ad62a63f0d8bdf3fe87e9b369
Chris Tai <christai100154@gmail.com>
2022-11-03 04:47:07 -0400

    Changed TextFlow to TextArea

--- src/main/java/Project3/GymManagerController.java
+++ src/main/java/Project3/GymManagerController.java
@@ -63,7 +63,7 @@ public class GymManagerController implements Initializable {
     @FXML
     Button printCounty;
     @FXML
-    TextFlow output1;
+    TextArea output1;
 
 
     @Override
@@ -98,14 +98,13 @@ public class GymManagerController implements Initializable {
         Member memToAdd = createMem();
         for (int i = 0; i < memData.size(); i++) {
             if (memData.returnList()[i].equals(memToAdd)) {
-                output1.getChildren().add(new Text(memToAdd.fullName() + " is already in the database.\n"));
+                output1.setText(memToAdd.fullName() + " is already in the database.\n");
                 return;
             }
         }
         if (!isOldEnough(memToAdd.dob())) return;
         if (memData.add(memToAdd)){
-            Text outputText = new Text(memToAdd.fullName() + " added.\n");
-            output1.getChildren().add(outputText);
+            output1.setText(memToAdd.fullName() + " added.\n");
         }
     }
 
@@ -152,19 +151,19 @@ public class GymManagerController implements Initializable {
         Date currentDate = new Date();
         String dob = checkDateOfBirth.toString();
         if (currentDate.compareTo(checkDateOfBirth) <= 0) {
-            output1.getChildren().add(new Text("DOB " + dob + ": cannot be today or a future date!\n"));
+            output1.setText("DOB " + dob + ": cannot be today or a future date!\n");
             return false;
         }
         if (currentDate.getYear() - checkDateOfBirth.getYear() < ADULT) {
-            output1.getChildren().add(new Text("DOB " + dob + ": must be 18 or older to join!\n"));
+            output1.setText("DOB " + dob + ": must be 18 or older to join!\n");
             return false;
         } else if (currentDate.getYear() - checkDateOfBirth.getYear() == ADULT) {
             if (currentDate.getMonth() < checkDateOfBirth.getMonth()) {
-                output1.getChildren().add(new Text("DOB " + dob + ": must be 18 or older to join!\n"));
+                output1.setText("DOB " + dob + ": must be 18 or older to join!\n");
                 return false;
             } else if (currentDate.getMonth() == checkDateOfBirth.getMonth()) {
                 if (currentDate.getDay() < checkDateOfBirth.getDay()) {
-                    output1.getChildren().add(new Text("DOB " + dob + ": must be 18 or older to join!\n"));
+                    output1.setText("DOB " + dob + ": must be 18 or older to join!\n");
                     return false;
                 }
             }
@@ -176,38 +175,33 @@ public class GymManagerController implements Initializable {
      * Prints out the members in the database in the current order
      */
     public void print(){
-        Text outputText = new Text(memData.print());
-        output1.getChildren().add(outputText);
+        output1.setText(memData.print());
     }
 
     /**
      * Prints out the members in the database sorted by county
      */
     public void printCounty(){
-        Text outputText = new Text(memData.printByCounty());
-        output1.getChildren().add(outputText);
+        output1.setText(memData.printByCounty());
     }
 
     /**
      * Prints out the members in the database sorted by expiration date
      */
     public void printExpire(){
-        Text outputText = new Text(memData.printByExpirationDate());
-        output1.getChildren().add(outputText);
+        output1.setText(memData.printByExpirationDate());
     }
     /**
      * Prints out the members in the database sorted by name
      */
     public void printName(){
-        Text outputText = new Text(memData.printByName());
-        output1.getChildren().add(outputText);
+        output1.setText(memData.printByName());
     }
     /**
      * Prints out the members in the database sorted by fee
      */
     public void printFee(){
-        Text outputText = new Text(memData.printWithFees());
-        output1.getChildren().add(outputText);
+        output1.setText(memData.printWithFees());
     }
 
 }
